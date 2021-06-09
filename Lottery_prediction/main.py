d43d322ddacbfcee46fb900f639ba3d66804676b
Jinduk <35905280+jindeok@users.noreply.github.com>
2021-06-09 23:03:31 +0900

    Add files via upload

--- Lottery_prediction/main.py
+++ Lottery_prediction/main.py
@@ -22,15 +22,15 @@ def parse_args():
     '''
     parser = argparse.ArgumentParser(description="Run Lottery prediction..")
     
-    parser.add_argument('--window', type = int, default = 3 , help = "time stamps")
+    parser.add_argument('--window', type = int, default = 1 , help = "time stamps")
     parser.add_argument('--data_dir', nargs='?', default='dataset/lottery_history.csv')
     
-    parser.add_argument('--mode', nargs='?', default='eval_model', help = "eval_model or predict")
+    parser.add_argument('--mode', nargs='?', default='back-test', help = "back-test or predict")
     parser.add_argument('--mode2', nargs='?', default='sampling', help = "greed or sampling")
-    parser.add_argument('--verb', nargs='?', default='not_verb', help = "verbose or not_verb")
+    parser.add_argument('--verb', nargs='?', default='not_verbose', help = "verbose or not_verb")
     
     parser.add_argument('--trial', type = int, default='10000', help = "how much trials to generate")
-    parser.add_argument('--training_length', type=float, default = 0.5)   
+    parser.add_argument('--training_length', type=float, default = 0.9)   
     
     return parser.parse_args()
 
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     prediction_number_set = LotteryLSTM.predict_lottery_numbers(args.mode2, args.trial)
     random_pred_set = LotteryLSTM.predict_randomely(args.trial)
     
-    if args.mode == 'eval_model':
+    if args.mode == 'back-test':
         LotteryLSTM.evaluate(prediction_number_set)
         print("---------Random baseline-------------")
         LotteryLSTM.evaluate(random_pred_set)
